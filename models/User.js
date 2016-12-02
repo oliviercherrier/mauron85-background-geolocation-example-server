bf572c26e1e3c7e9f4b077bb4982131562d69544
oliviercherrier <olivier.cherrier@gmail.com>
2016-12-02 14:31:59 +0100

    Implement first path service test version

--- models/User.js
+++ models/User.js
@@ -4,7 +4,13 @@ var UserSchema = new mongoose.Schema({
   _id     : Number,
   first_name: String,
   last_name: String,
+  phone_id: String,
   paths : [{ type: Number, ref: 'Path' }]
 });
 
+UserSchema.statics.findByName = function(name, cb) {
+  return this.find({ phone_id: new RegExp(name, 'i') }, cb);
+};
+
 module.exports = mongoose.model('User', UserSchema);
+  
---
+++ routes/path.js
@@ -0,0 +1,32 @@
+var express = require('express');
+var router = express.Router();
+
+var Path = require('../models/Path.js');
+var User = require('../models/User.js')
+
+/* GET users listing. */
+
+
+// Prend en paramètre un identifiant de téléphone et retourne le path créé
+// req.body
+router.post('/', function(req, res, next) {
+    console.log("BODY:");
+    console.log(req.body);
+    // Test if user exist
+    User.findByName(req.body.Phone_uuid, function(err, user) {
+        console.log(user);
+        // If not create it
+        if(user.length == 0){
+            User.create({phone_id: req.body.Phone_uuid}, function(error, doc) {
+                if (err) {console.log(err);}
+            });
+        }
+    });
+    
+    /*var path = Path.create({is_active: true}, function (err, post) {
+        if (err) return next(err);
+        res.json(post);
+    });*/
+});
+
+module.exports = router;
