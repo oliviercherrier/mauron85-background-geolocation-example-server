e07788841cf317020e7ea4b441df1eff9e1657aa
oliviercherrier <olivier.cherrier@gmail.com>
2016-12-21 16:38:04 +0100

    CORS

--- app.js
+++ app.js
@@ -16,6 +16,15 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+
+// Enable Cross Origin ressource sharing
+app.all('*', function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, authorization');
+  next();
+});
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
