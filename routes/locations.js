e0829f8a73981b0e0b9359024249ad1061e930f9
oliviercherrier <olivier.cherrier@gmail.com>
2016-12-21 19:48:03 +0100

    user/id return simple user ir 204

--- routes/locations.js
+++ routes/locations.js
@@ -7,16 +7,16 @@ var User = require('../models/User.js');
 router.post('/', function(req, response){
     Location.create(req.body, function(err,createdLocations){
         if(err) return next(err);
-        var users = User.findByUuid(req.headers.device_uuid, function(err, users) {})
+        var user = User.findByUuid(req.headers.device_uuid, function(err, users) {})
         .populate({path: 'paths', match: {is_active: true}})
-        .exec(function(err, users){
-            if (users[0].paths.length > 0){
+        .exec(function(err, user){
+            if (user.paths.length > 0){
                 var i = 0;
                 for (i = 0; i < createdLocations.length; i++){
-                    users[0].paths[0].locations.push(createdLocations[i]);
+                    user.paths[0].locations.push(createdLocations[i]);
                 }
-                users[0].paths[0].save(function(error,doc) {if (err) {console.log(err);}});
-                response.sendStatus(200);
+                user.paths[0].save(function(error,doc) {if (err) {console.log(err);}});
+                response.json(user);
             }
             else{
                 response.sendStatus(409);
